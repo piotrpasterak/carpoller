c27a0e3f096822e859b200691a06e7256ba9f66e
martin7070 <41266190+martin7070@users.noreply.github.com>
2019-06-07 00:02:59 +0200

    calendar menu, return buttons,

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -9,17 +9,27 @@ import { HttpClient } from '@angular/common/http';
 export class CalendarComponent implements OnInit {
   holidaysdata: string;
   holidaysList: string[];
+  countries = [{ id: 1, name: 'US' }, { id: 2, name: 'PL' }, { id: 3, name: 'DE' }];
+  selectedCountry = 2;
   loading: boolean;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
+  onSelect(event: Event) {
+    const selector = event.target as HTMLSelectElement;
+    const num = selector.value as unknown;
+    this.selectedCountry = num as number;
+    this.holidaysList = [];
+  }
   makeRequest(): void {
     this.loading = true;
     this.http
       .get(
-        'https://calendarific.com/api/v2/holidays?&api_key=038a4d5f3c1553c33f1d637289f1b518cd92a5aef5f6cca1febd7a889c11a8f3&country=PL&year=2019&type=national',
+        'https://calendarific.com/api/v2/holidays?&api_key=038a4d5f3c1553c33f1d637289f1b518cd92a5aef5f6cca1febd7a889c11a8f3&country=' +
+          this.countries[this.selectedCountry - 1].name +
+          '&year=2019&type=national',
         { responseType: 'json' }
       )
       .subscribe(response => {
