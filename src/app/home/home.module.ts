acf0bdc02be4576b0b0e1d0db5c9a21e9c6f1ee8
piotrp <38702369+piotrpasterak@users.noreply.github.com>
2019-06-09 22:17:17 +0200

    Revert "Revert "DS Backend logic - fetching observables from DB & nomination mechanism""

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,9 +7,11 @@ import { SharedModule } from '@app/shared';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { QuoteService } from './quote.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, CoreModule, SharedModule, HomeRoutingModule],
+  imports: [CommonModule, TranslateModule, CoreModule, SharedModule, HomeRoutingModule, BrowserModule, FormsModule],
   declarations: [HomeComponent],
   providers: [QuoteService]
 })
